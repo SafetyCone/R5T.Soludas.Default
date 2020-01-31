21b93de8d78b877d6a8f4c7e1fe35d9030732f9a
MinexAutomation <minex.automation@gmail.com>
2020-01-30 23:47:17 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Soludas.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Cambridge.Types;
+using R5T.Dacia;
+using R5T.Vandalia;
+
+
+namespace R5T.Soludas.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileValueEqualityComparer"/> implementation of <see cref="IVisualStudioSolutionFileValueEqualityComparer"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultVisualStudioSolutionFileValueEqualityComparer(this IServiceCollection services,
+            ServiceAction<IValueEqualityComparer<SolutionFile>> addSolutionFileValueEqualityComparer)
+        {
+            services
+                .AddSingleton<IVisualStudioSolutionFileValueEqualityComparer, VisualStudioSolutionFileValueEqualityComparer>()
+                .RunServiceAction(addSolutionFileValueEqualityComparer)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileValueEqualityComparer"/> implementation of <see cref="IVisualStudioSolutionFileValueEqualityComparer"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileValueEqualityComparer> AddDefaultVisualStudioSolutionFileValueEqualityComparerAction(this IServiceCollection services,
+            ServiceAction<IValueEqualityComparer<SolutionFile>> addSolutionFileValueEqualityComparer)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileValueEqualityComparer>(() => services.AddDefaultVisualStudioSolutionFileValueEqualityComparer(
+                addSolutionFileValueEqualityComparer));
+            return serviceAction;
+        }
+    }
+}
