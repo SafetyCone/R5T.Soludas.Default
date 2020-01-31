d3576eda950456af5d77cdeb10d57a013be4b969
MinexAutomation <minex.automation@gmail.com>
2020-01-30 23:35:42 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Soludas.Default.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Soludas.Default.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Soludas.Default/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Soludas.Default
+{
+    public class Class1
+    {
+    }
+}
