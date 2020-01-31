1249408e6aa7b4c6af264a7b45cdbe8bce9c4cb9
MinexAutomation <minex.automation@gmail.com>
2020-01-30 23:46:51 -0800

    Added VisualStudioSolutionFileValueEqualityComparer implementation of IVisualStudioSolutionFileValueEqualityComparer.

---
+++ source/R5T.Soludas.Default/Code/Services/Implementations/VisualStudioSolutionFileValueEqualityComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using R5T.Cambridge.Types;
+using R5T.Vandalia;
+
+
+namespace R5T.Soludas.Default
+{
+    public class VisualStudioSolutionFileValueEqualityComparer : IVisualStudioSolutionFileValueEqualityComparer
+    {
+        private IValueEqualityComparer<SolutionFile> SolutionFileValueEqualityComparer { get; }
+
+
+        public VisualStudioSolutionFileValueEqualityComparer(IValueEqualityComparer<SolutionFile> solutionFileValueEqualityComparer)
+        {
+            this.SolutionFileValueEqualityComparer = solutionFileValueEqualityComparer;
+        }
+
+        public bool Equals(SolutionFile x, SolutionFile y)
+        {
+            var equals = this.SolutionFileValueEqualityComparer.Equals(x, y);
+            return equals;
+        }
+    }
+}
